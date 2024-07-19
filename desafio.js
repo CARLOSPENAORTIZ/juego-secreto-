1438dad1f0a48cbba6c3ed9c3d9821b259e8ae11
CARLOSPENAORTIZ <karloshernando92@gmail.com>
2024-07-19 00:09:26 -0500

    versión final juego numero secreto

---
+++ desafio.js
@@ -0,0 +1,12 @@
+let  listaGenerica =[]
+
+let LenguajesDeProgramacion =[ 'JavaScript', 'C', 'C++', 'Kotlin',  'Python' ];
+lenguagesDeProgramacion.push('Java', 'Ruby', 'GoLang');
+
+function mostrarLenguagesSeparadamente() {
+    for (let i = 0; i < lenguagesDeProgramacion.length; i++) {
+      console.log(lenguagesDeProgramacion[i]);
+    }
+  }
+  
+  mostrarLenguagesSeparadamente();
